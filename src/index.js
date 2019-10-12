a65de8d43359248803dcfbe8aaf533762c938db7
Vladislav0101 <by.vlad01@mail.ru>
2019-10-12 16:40:07 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let stack = [];
+
+    for(let i = 0; i < str.length; i++){
+      if(str[i] == '(' || str[i] == '{' || str[i] == '['){
+        stack.push(str[i]);
+      } 
+      else if(str[i] == ')') {
+        if(stack.pop() != '(') return false;
+      } 
+      else if(str[i] == '}'){
+        if(stack.pop() != '{') return false;
+      } 
+      else if(str[i] == ']'){
+        if(stack.pop() != '[') return false;
+      } 
+    }
+    
+    return(!stack.length);
 }
